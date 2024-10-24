8a776d5b5f02b03189705b79f0603884be6b908d
RoxMS <roxanams@tamu.edu>
2024-10-24 15:08:16 -0500

    Finished BM25 Function

--- bm25.py
+++ bm25.py
@@ -1,8 +1,10 @@
 import cleaning
+import math
 
 def BM25(q):
     tracked_electives = cleaning.clean()
-    tfs = cleaning.term_frequencies()
+    tfs = cleaning.term_frequencies()[0]
+    dfs = cleaning.term_frequencies()[1]
     lengths = cleaning.docLengths()
     avg_lengths = cleaning.totalAvgDocLen()
     query = cleaning.clean_query(q)
@@ -13,14 +15,19 @@ def BM25(q):
     bbody = 0.2
     k1 = 1.3
 
-    for word in query {
-        for 
+    bm25dict = {} #dictionary to store the bm25 scores for each document
+    for doc in tfs.keys():
+        bm25 = 0
+        for word in query:
+            tftitle = (tfs[doc]['title'][word]) / (1-btitle) + (btitle * (lengths[doc]['title'] / avg_lengths['title']))
+            tfbody = (tfs[doc]['body'][word]) / (1-bbody) + (bbody * (lengths[doc]['body'] / avg_lengths['body']))
 
-                # length['title'] /= (1-titleweight) + (titleweight * (lengths.get(d).get(type) / avgLengths.get(type)))
+            tfnormalized = titleweight * tftitle + bodyweight * tfbody
+            bm25 += math.log((len(tracked_electives)) / len(dfs[word])) * ((k1 + 1) * tfnormalized) / (k1 + tfnormalized)
+        
+        bm25dict[doc] = bm25  
 
-    }
+    return sorted(bm25dict, key = bm25dict.get, reverse = True) 
 
 
-    #iterate through tfs
-        #normalize tfs per zone (tfi): tf = tf / (1-b) + b * (lengths.get(d).get(type) / avgLengths.get(type))
-
+#Make function that calls BM25 and returns the top X results in each track 
