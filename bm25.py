299a060cb002ef9f37cd6d8fcc3d2b40b1862c1d
RoxMS <roxanams@tamu.edu>
2024-10-31 13:43:25 -0500

    Testing with Prints

--- bm25.py
+++ bm25.py
@@ -19,33 +19,40 @@ def BM25(q):
     for doc in tfs.keys():
         bm25 = 0
         for word in query:
-            print(word)
             if word in tfs[doc]['title'].keys():
                 tftitle = (tfs[doc]['title'][word]) / (1-btitle) + (btitle * (lengths[doc]['title'] / avg_lengths['title']))
+                    
             else:
                 tftitle = 0
 
             if word in tfs[doc]['body'].keys():
                 tfbody = (tfs[doc]['body'][word]) / (1-bbody) + (bbody * (lengths[doc]['body'] / avg_lengths['body']))
+
             else:
                 tfbody = 0
 
             tfnormalized = titleweight * tftitle + bodyweight * tfbody
-
+            
             if word in dfs.keys():
+                if((len(tracked_electives)) / len(dfs[word]) != 0):
+                    print(len(tracked_electives))
                 bm25 += math.log((len(tracked_electives)) / len(dfs[word])) * ((k1 + 1) * tfnormalized) / (k1 + tfnormalized)
         
         bm25dict[doc] = bm25  
-
-    return sorted(bm25dict, key = bm25dict.get, reverse = True) 
-
+        
+    return bm25dict
+"""     return sorted(bm25dict, key = bm25dict.get, reverse = True) 
+ """
 
 #Make function that calls BM25 and returns the top X results in each track 
 
 
-BM25("data science, machine learning, python")
-""" tracked_electives = cleaning.clean()
+BM25("software")
+""" print(BM25("software"))
+ """""" tracked_electives = cleaning.clean()
 tfs = cleaning.term_frequencies(tracked_electives)[0]
 dfs = cleaning.term_frequencies(tracked_electives)[1]
 
 print(dfs) """
+
+
